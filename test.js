b0d4044a55f94de77f43092324b6790d9838b1f8
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2016-05-19 19:12:20 +0200

    Specify formatting

--- test.js
+++ test.js
@@ -1,3 +1,6 @@
+'use strict'; // eslint-disable-line strict
+  // http://stackoverflow.com/q/33063206
+
 // Format my own tests, FTW!
 require('.');
 
@@ -40,5 +43,96 @@ test('Strips off data for machines', (is) => {
 
   mockConsole.log('TAP version 13');
   mockConsole.log('1..178');
+  is.end();
+});
+
+test('Adds some space around test titles', (is) => {
+  is.plan(2);
+
+  const title = 'Whatever';
+  const log = (message) => {
+    is.notOk(/#/.test(naked(message)),
+      'strips the `# ` off the beginning'
+    );
+
+    is.equal(naked(message), `\n${title}\n`,
+      'adds a newline above and below'
+    );
+  };
+
+  const mockConsole = { log };
+  proxyquire('.', { global: { console: mockConsole } });
+
+  mockConsole.log(`# ${title}`);
+  is.end();
+});
+
+test('Changes an `ok <number>` into an indented `✔`', (is) => {
+  is.plan(2);
+
+  const comment = 'Whatever';
+  const log = (message) => {
+    is.notOk(/^ok/.test(naked(message)),
+      'strips the `ok <number> ` off the beginning'
+    );
+
+    is.equal(naked(message), `  ✔ ${comment}`,
+      'adds an indented ✔ instead'
+    );
+  };
+
+  const mockConsole = { log };
+  proxyquire('.', { global: { console: mockConsole } });
+
+  mockConsole.log(`ok 17 ${comment}`);
+  is.end();
+});
+
+test('Changes a `not ok <number>` into an indented `✘`', (is) => {
+  is.plan(2);
+
+  const comment = 'Whatever';
+  const log = (message) => {
+    is.notOk(/^not ok/.test(naked(message)),
+      'strips the `not ok <number> ` off the beginning'
+    );
+
+    is.equal(naked(message), `  ✘ ${comment}`,
+      'adds an indented ✘ instead'
+    );
+  };
+
+  const mockConsole = { log };
+  proxyquire('.', { global: { console: mockConsole } });
+
+  mockConsole.log(`not ok 58 ${comment}`);
+  is.end();
+});
+
+test('Cleans up the test footer', (is) => {
+  const footer = [
+    '# tests 9',
+    '# pass  9',
+    '# fail  0',
+    '',
+    '# ok',
+  ];
+
+  is.plan(footer.length);
+
+  let counter = 0;
+  const log = (message) => {
+    is.equal(
+      naked(message),
+      footer[counter].replace(/^# /, ''),
+      `strips the \`# \` off in line n° ${counter + 1}`
+    );
+    counter++;
+  };
+
+  const mockConsole = { log };
+  proxyquire('.', { global: { console: mockConsole } });
+
+  footer.forEach(message => mockConsole.log(message));
   is.end();
 });
