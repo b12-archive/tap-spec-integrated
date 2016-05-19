e3ab77ae23159ca5ae60ddbca37ba779c26661cd
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2016-05-19 18:08:16 +0200

    Specify basic behavior

--- test.js
+++ test.js
@@ -1,3 +1,28 @@
 const test = require('tape-catch');
+const proxyquire = require('proxyquire');
+const naked = require('strip-ansi');
 
-const tapSpecIntegrated = require('.');
+test('Doesn’t break `console.log`', (is) => {
+  const logMessage = 'just logging stuff';
+  const extraLogData = {};
+  const log = (message, otherArg) => {
+    is.equal(naked(message), logMessage,
+      'leaves non-TAP log messages intact'
+    );
+
+    is.equal(otherArg, extraLogData,
+      'leaves extra log data intact'
+    );
+
+    is.end();
+  };
+
+  const mockConsole = { log };
+  proxyquire('.', { global: { console: mockConsole } });
+
+  is.notEqual(mockConsole.log, log,
+    'wraps the native `console.log`'
+  );
+
+  mockConsole.log(logMessage, extraLogData);
+});
